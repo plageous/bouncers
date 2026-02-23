b6477638688fb6bed9cd265632ee15f8e6f4b38d
KJ <kjmiclea@gmail.com>
2026-02-23 01:42:03 +0000

    Refactored average function to use Bouncer class.

--- src/main.cpp
+++ src/main.cpp
@@ -20,70 +20,86 @@ static constexpr bn::fixed BASE_SPEED = 2;
 // Maximum number of bouncers on screen at once
 static constexpr int MAX_BOUNCERS = 20;
 
-class Bouncer {
-    public: 
-        bn::sprite_ptr sprite = bn::sprite_items::dot.create_sprite();
-        bn::fixed x_speed = BASE_SPEED;
+class Bouncer
+{
+public:
+    bn::sprite_ptr sprite = bn::sprite_items::dot.create_sprite();
+    bn::fixed x_speed = BASE_SPEED;
 
-        void update() {
-            bn::fixed x = sprite.x();
+    void update()
+    {
+        bn::fixed x = sprite.x();
 
-            // Update x position by adding speed
-            x += x_speed;
+        // Update x position by adding speed
+        x += x_speed;
 
-            // If we've gone off the screen on the right
-            if(x > MAX_X) {
-                // Snap back to screen and reverse direction
-                x = MAX_X;
-                x_speed *=-1;
-            }
-            // If we've gone off the screen on the left
-            if(x < MIN_X) {
-                // Snap back to screen and reverse direction
-                x = MIN_X;
-                x_speed *= -1;
+        // If we've gone off the screen on the right
+        if (x > MAX_X)
+        {
+            // Snap back to screen and reverse direction
+            x = MAX_X;
+            x_speed *= -1;
+        }
+        // If we've gone off the screen on the left
+        if (x < MIN_X)
+        {
+            // Snap back to screen and reverse direction
+            x = MIN_X;
+            x_speed *= -1;
         }
 
         sprite.set_x(x);
     }
 };
 
-bn::fixed average_x(bn::vector<Bouncer, MAX_BOUNCERS>& bouncers) {
-// Add all x positions together
+bn::fixed average_x(bn::vector<Bouncer, MAX_BOUNCERS> &bouncers)
+{
+    // Add all x positions together
     bn::fixed x_sum = 0;
-    for(Bouncer bouncer : bouncers) {
-        x_sum += bouncer.sprite.x();
+    for (Bouncer bouncer : bouncers)
+    {
+        x_sum += bouncer.x_speed;
     }
 
-    bn::fixed x_average= x_sum;
+    bn::fixed x_average = x_sum;
 
     // Only divide if we have 1 or more
     // Prevents division by 0
-    if(bouncers.size() > 0) {
+    if (bouncers.size() > 0)
+    {
         x_average /= bouncers.size();
     }
 
     return x_sum;
 }
 
-void add_bouncer(bn::vector<Bouncer, MAX_BOUNCERS>& bouncers) {
+void add_bouncer(bn::vector<Bouncer, MAX_BOUNCERS> &bouncers)
+{
     // Only add if we're below the maximum
-    if(bouncers.size() < bouncers.max_size()) {
+    if (bouncers.size() < bouncers.max_size())
+    {
         bouncers.push_back(Bouncer());
     }
 }
 
 int main() {
     bn::core::init();
-    
+
     bn::vector<Bouncer, MAX_BOUNCERS> bouncers = {};
 
-    while(true) {
-        if (bn::keypad::a_pressed()) { add_bouncer(bouncers); }
-        if (bn::keypad::b_pressed()) { /* BN_LOG("Average x: ", average_x(bouncers)); */ }
+    while (true)
+    {
+        if (bn::keypad::a_pressed()) {
+            add_bouncer(bouncers);
+        }
+        if (bn::keypad::b_pressed()) {
+            BN_LOG("Average x: ", average_x(bouncers));
+        }
 
         // for each bouncer
-        for (Bouncer& bouncer : bouncers) { bouncer.update(); }
+        for (Bouncer &bouncer : bouncers) {
+            bouncer.update();
+        }
 
         bn::core::update();
     }
